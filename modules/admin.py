67471572cde4ee2b18ecaeeba23bcc1b57536095
Nexus <nexus007@gmx.de>
2009-04-07 00:42:55 +0200

    rehash stuff which doesnt work yet but the hard part is done... i think...

--- modules/admin.py
+++ modules/admin.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+    
 import sys
 from modules import IRCBotMod
+import modules
 from utils import nick
 
 class AdminMod(IRCBotMod):
@@ -13,6 +14,10 @@ class AdminMod(IRCBotMod):
         IRCBotMod.__init__(self,head)
         self.handleInput = self.handler
         self.adminpass = adminpass
+        self.storage["bot"] = self.head
+    def rehashlist(self):
+        return [self.adminpass]
+        
     def handler(self,matchlist):
         host = matchlist[0]
         target = matchlist[1] if (matchlist[1] != self.head.nickname) else nick(host)
@@ -51,9 +56,26 @@ class AdminMod(IRCBotMod):
                 self.head.sendraw("PRIVMSG " + target + " :" + str(len(self.head.spamqueue.queue)) + " items in spamqueue.")
             elif (command == "!rehash"):
                 self.head.sendraw("PRIVMSG " + target + " :Reimporting code...",)
+                if (not text):
+                    for x in self.head.MODLIST:
+                        self.rehashModule(x)
+                else:
+                    self.rehashModule(self.head.MODLIST[text])
                 if ("pyhkal" in sys.modules):
                     sys.modules.pop("pyhkal")
                 instance = self.head
                 import pyhkal
                 instance.__class__ = pyhkal.IRCBot
                 pyhkal.main(instance)
+                
+    def rehashModule(self,module):
+        sys.modules.pop(str(module.__module__))
+        newmod = __import__(module.__module__,globals(),locals(),[module.__class__.__name__])
+        bot.MODLIST.remove(module.__class__.__name__)
+        globals()[str(module.__module__)] = newmod
+        try:
+            #TODO: fix this hack // simplify module reloading with constructorparameters
+            self.head.addModule(newmod,module.rehashlist())
+        except AttributeError:
+            self.head.addModule(newmod)
+        
