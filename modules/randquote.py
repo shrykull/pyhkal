e142cb3619ee52dcf33018b3e202f904983b9358
Nexus <nexus007@gmx.de>
2009-04-07 23:56:53 +0200

    ehm hello randquote, upload yourself (i begin to hate git)

---
+++ modules/randquote.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from modules import IRCBotMod
+from datetime import time
+from utils import file2obj, obj2file
+from random import randint
+
+class RandquoteMod(IRCBotMod):
+    regexpattern = r':(.+)!.+ PRIVMSG {0} :(.+)'
+    quotepercentage = 2
+    def __init__(self,head,filename):
+        IRCBotMod.__init__(self,head)
+        #this module has to be limited to the channel due to the possibility of quoting admin logins etc.
+        self.regexpattern = self.regexpattern.format(head.mainchannel)
+        self.handleInput = self.handler
+        self.filename = filename
+        try:
+            self.quotedict = file2obj(self.filename)
+        except IOError:
+            self.quotedict = {}
+    def rehashlist(self):
+        return [self.filename]
+        
+    def handler(self,matchlist):
+        #self.head.sendMsg(self.head.mainchannel,"foo boo randquote rehash boo bobo")
+        if ((randint(0,100) < self.quotepercentage) or (matchlist[0] not in self.quotedict)):
+            self.quotedict[matchlist[0]] = quote(matchlist[0],time(),matchlist[1])
+            obj2file(self.quotedict,self.filename)
+
+class quote(object):
+    def __init__(self,nick,time,quotestring):
+        self.nick = nick
+        self.time = time
+        self.quotestring = quotestring
